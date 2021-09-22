aa4fcf106642e5d364044eb767689481dc9f657a
nickbron <77615583+nickbron@users.noreply.github.com>
2021-09-22 23:54:04 +0300

    create hooks

--- src/Componets/App.jsx
+++ src/Componets/App.jsx
@@ -1,85 +1,63 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import shortid from 'shortid';
 import ContactForm from 'Componets/ContactForm/ContactForm';
 import ContactList from 'Componets/ContactList/ContactList';
 import Filter from 'Componets/Filter/Filter';
 import { Card } from './App.styled';
 
-export default class App extends Component {
-  state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-    name: '',
-    number: '',
-  };
+export default function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  addNewContact = person => {
-    const contact = {
-      id: shortid.generate(),
-      name: person.name,
-      number: person.number,
-    };
-    if (this.state.contacts.find(contact => contact.name === person.name)) {
-      alert(`${person.name} is already in contacts.`);
-      return;
-    }
-    this.setState(prevState => ({
-      contacts: [contact, ...prevState.contacts],
-    }));
-  };
-
-  handleInputChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-  };
-
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(person => person.id !== id),
-    }));
-  };
-
-  componentDidMount() {
+  useEffect(() => {
     const contacts = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(contacts);
     if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
+      setContacts(parsedContacts);
     }
-  }
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      console.log('обновилось');
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleInputChange = e => {
+    setFilter(e.target.value);
+  };
+
+  const addNewContact = (name, number) => {
+    const contact = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+    if (contacts.find(contact => contact.name === name)) {
+      alert(`${name} is already in contacts.`);
+      return;
     }
-  }
+    setContacts([contact, ...contacts]);
+  };
 
-  render() {
-    const { contacts, filter } = this.state;
-    const normalizedFilter = filter.toLowerCase();
-    const findContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter),
-    );
+  const deleteContact = id => {
+    setContacts(contacts.filter(prevState => prevState.id !== id));
+    setFilter('');
+  };
 
-    return (
-      <Card>
-        <h1 className="title">Phonebook</h1>
+  const normalizedFilter = filter.toLowerCase();
+  const findContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter),
+  );
 
-        <ContactForm onAdd={this.addNewContact} />
+  return (
+    <Card>
+      <h1 className="title">Phonebook</h1>
 
-        <h2 className="title">Contacts</h2>
+      <ContactForm onAdd={addNewContact} />
 
-        <Filter filter={filter} onChange={this.handleInputChange} />
-        <ContactList
-          contacts={findContacts}
-          onDeleteContact={this.deleteContact}
-        />
-      </Card>
-    );
-  }
+      <h2 className="title">Contacts</h2>
+
+      <Filter filter={filter} onChange={handleInputChange} />
+      <ContactList contacts={findContacts} onDeleteContact={deleteContact} />
+    </Card>
+  );
 }
--- src/Componets/ContactForm/ContactForm.jsx
+++ src/Componets/ContactForm/ContactForm.jsx
@@ -1,56 +1,61 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Button } from './ContactForm.styled';
 import { Input } from './ContactForm.styled';
 import { Form } from './ContactForm.styled';
 
-export default class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
+export default function ContactForm({ onAdd }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        break;
+    }
   };
 
-  handleInputChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onAdd(this.state);
-    this.reset();
+    onAdd(name, number);
+    reset();
   };
 
-  reset = () =>
-    this.setState({
-      name: '',
-      number: '',
-    });
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
 
-  render() {
-    return (
-      <div>
-        <Form onSubmit={this.handleSubmit}>
-          <Input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            value={this.state.name}
-            onChange={this.handleInputChange}
-            required
-          />
-          <Input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-            value={this.state.number}
-            onChange={this.handleInputChange}
-            required
-          />
-          <Button type="submit">Add contact</Button>
-        </Form>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          value={name}
+          onChange={handleInputChange}
+          required
+        />
+        <Input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          value={number}
+          onChange={handleInputChange}
+          required
+        />
+        <Button type="submit">Add contact</Button>
+      </Form>
+    </div>
+  );
 }
